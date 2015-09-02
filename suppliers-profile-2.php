e43864317b7e36b153c741f709068555e9803f1e
Tom Wicks <tom@miln.co>
2015-09-02 16:31:02 +0100

    .hidden

---
+++ suppliers-profile-2.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8 />
+  <meta name="description" content="description">
+
+  <title>Wasserman Experience Intranet</title>
+
+  <link rel="stylesheet" media="screen" href="style.css" />
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable="no"">
+
+  <!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+</head>
+
+<body>
+
+  <?php include 'includes/top-message.php';?>
+
+  <?php include 'includes/sidebar.php';?>
+
+  <div class="layout-content-container">
+
+    <header class="header-type-two">
+
+      <div class="layout-container">
+
+        <a href="#" class="js-trigger-sidebar"><i class="material-icons md-18">menu</i></a>
+
+        <h3 class="type-light">Suppliers</h3>
+
+        <ul class="element-top-options-right">
+
+          <li><a class="js-trigger-notifications" title="js-add-supplier"><i class="material-icons link-arrow-bottom-active">add</i></a></li>
+
+          <li>
+
+            <a class="js-trigger-notifications" title="js-notifications">
+
+              <i class="material-icons link-arrow-bottom-active">flag</i>
+
+              <span class="element-notification">2</span>
+
+            </a> <!-- js-trigger-notifications -->
+
+          </li> 
+
+        </ul>  <!-- element-top-options-right -->
+
+      </div> <!-- layout-container -->
+
+    </header> <!-- header-type-two -->
+
+    <div class="article-notification-box" id="js-notifications">
+
+      <h5 class="type-light">Notifications</h5>
+
+      <ul class="list-style-six">
+
+        <a><li><span>Dave Clayton has completed stage one of the suppliers Sign Up Process</span><span class="type-bold"> Review Details</span></li></a>
+
+        <a><li><span>Dave Clayton has completed stage one of the suppliers Sign Up Process</span><span class="type-bold"> Review Details</span></li></a>
+
+        <a><li><span>View All</span></li></a>
+
+      </ul> <!-- list-style-six -->
+
+    </div> <!-- article-notification-box -->
+
+    <div class="article-notification-box" id="js-add-supplier">
+
+      <h5 class="type-light">Add Suppliers</h5>
+
+        <p>Send your personal link to your potential supplier so they can sign the NDA.</p>
+
+        <span class="element-link-box">https://intranet.wassermanexperience.com/suppliers/register/myemail.co.uk</span>
+
+        <a class="link-style-white"><i class="material-icons md-18">content_paste</i>Copy To Clipboard</a>
+
+    </div> <!-- article-notification-box -->
+
+    <article class="article-left-pane">
+
+      <div class="element-category-button element-category-button-active">
+
+        <h5>Service Type</h5>
+
+      </div> <!-- element-category-button -->
+
+      <div class="element-category-button">
+
+        <h5>Filter Options</h5>
+
+      </div> <!-- element-category-button -->
+
+      <div class="element-category-slider-holder">
+
+        <div class="element-category-slider element-panel-one">
+
+          <ul class="list-style-three">
+
+            <li class="list-three-item js-trigger-panel-two" title="list-item-one">
+
+              <h5>Event Services and Infrastructure</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_right</i>
+
+            </li> <!-- list-three-item -->
+
+            <li class="list-three-item js-trigger-panel-two" title="list-item-two">
+
+              <h5>Event Technology</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_right</i>
+
+            </li> <!-- list-three-item -->
+
+            <li class="list-three-item js-trigger-panel-two" title="list-item-three">
+
+              <h5>Event Staff</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_right</i>
+
+            </li> <!-- list-three-item -->
+
+            <li class="list-three-item js-trigger-panel-two" title="list-item-four">
+
+              <h5>Digital</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_right</i>
+
+            </li> <!-- list-three-item -->
+
+            <li class="list-three-item js-trigger-panel-two" title="list-item-five">
+
+              <h5>Venues</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_right</i>
+
+            </li> <!-- list-three-item -->
+
+          </ul> <!-- list-style-three -->
+
+        </div> <!-- element-category-slider -->
+
+        <div class="element-category-slider element-panel-two">
+
+          <div class="element-category-item" id="list-item-one">
+
+            <div class="element-category-title js-trigger-panel-one">
+
+               <h5>Event Services and Infrastructure</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_left</i>
+
+            </div> <!-- element-category-title -->
+
+              <ul class="list-style-four">
+
+                <li class="form-radio-button-active">
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins / Steel Stores</span>
+
+                </li>
+
+                <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins / Steel Stores</span>
+
+                </li>
+
+                 <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins</span>
+
+                </li>
+
+                 <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins</span>
+
+                </li>
+
+                <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins</span>
+
+                </li>
+
+                <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins</span>
+
+                </li>
+                
+                <li>
+
+                  <span class="form-radio-button"></span>
+
+                  <span class="type-style-one">Portacabins</span>
+
+                </li>
+              
+              </ul> <!-- list-style-four -->
+
+          </div> <!-- element-category-item -->
+
+          <div class="element-category-item" id="list-item-two">
+
+            <div class="element-category-title js-trigger-panel-one">
+
+               <h5>Event Technology</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_left</i>
+
+            </div> <!-- element-category-title -->
+
+          </div> <!-- element-category-item -->
+
+          <div class="element-category-item" id="list-item-three">
+
+            <div class="element-category-title js-trigger-panel-one">
+
+               <h5>Event Staff</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_left</i>
+
+            </div> <!-- element-category-title -->
+
+          </div> <!-- element-category-item -->
+
+          <div class="element-category-item" id="list-item-four">
+
+            <div class="element-category-title js-trigger-panel-one">
+
+               <h5>Digital</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_left</i>
+
+            </div> <!-- element-category-title -->
+
+          </div> <!-- element-category-item -->
+
+          <div class="element-category-item" id="list-item-five">
+
+            <div class="element-category-title js-trigger-panel-one">
+
+               <h5>Venues</h5>
+
+              <i class="material-icons link-icons-white md-18">keyboard_arrow_left</i>
+
+            </div> <!-- element-category-title -->
+
+          </div> <!-- element-category-item -->
+          
+        </div> <!-- element-category-slider -->
+
+      </div>  <!-- element-category-slider-holder -->
+ 
+    </article> <!-- article-filter -->
+
+    <article class="article-main-content">
+
+        <section class="article-search">
+
+          <div class="layout-container">
+
+            <form class="form-search">
+
+              <i class="material-icons md-18">search</i>
+
+              <input type="search" placeholder=Search>
+
+            </form>  <!-- form-search -->
+
+          </div> <!-- layout-container -->
+
+        </section>  <!-- article-search -->
+
+        <section class="article-tags">
+
+          <div class="layout-container">
+
+            <div class="element-tag">
+
+              <span>Designers</span>
+
+              <i class="material-icons link-icons-white md-18">clear</i>
+
+            </div> <!-- element-tag -->
+
+            <div class="element-tag">
+
+              <span>Used Before</span>
+
+              <i class="material-icons link-icons-white md-18">clear</i>
+
+            </div>  <!-- element-tag -->
+
+          </div> <!-- layout-container -->
+
+        </section> <!-- article-tags -->
+
+        <article class="article-supplier-card">
+
+          <div class="element-supplier-header">
+
+            <h3>Miln Ltd</h3>
+
+            <span>1 of 17</span>
+
+            <div class="element-supplier-switcher">
+
+              <i class="material-icons">keyboard_arrow_left</i>
+
+              <i class="material-icons">keyboard_arrow_right</i>
+
+            </div>
+
+            <div class="element-supplier-basics">
+
+              <h5>We design digital products which help your organisation do great work. Underpinned by the latest technology and user-centric design our aim is to bring a human touch to the digital world.</h5>
+
+            </div>  <!-- element-supplier-basics -->
+
+          </div> <!-- element-supplier-header -->
+
+        </article>  <!-- article-supplier-card -->
+
+    </article> <!-- article-main-content -->
+
+  </div> <!-- layout-content-container -->
+
+  <script type="text/javascript" src="MyFontsWebfontsKit.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+</html>
